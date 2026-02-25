690ab09868e7cb803b406c33292b2f6572781fb3
abhii012 <you@example.com>
2026-02-25 14:06:38 +0530

    fix api path

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = import.meta.env.VITE_API_URL;
 
-export const getProducts = () => axios.get(`${API}/products`);
-export const createProduct = data => axios.post(`${API}/products`, data);
-export const updateProduct = (id, data) => axios.put(`${API}/products/${id}`, data);
-export const deleteProduct = id => axios.delete(`${API}/products/${id}`);
-export const getProduct = id => axios.get(`${API}/products/${id}`);
+export const getProducts = () => axios.get(`${API}/api/products`);
+export const createProduct = data => axios.post(`${API}/api/products`, data);
+export const updateProduct = (id, data) => axios.put(`${API}/api/products/${id}`, data);
+export const deleteProduct = id => axios.delete(`${API}/api/products/${id}`);
+export const getProduct = id => axios.get(`${API}/api/products/${id}`);
