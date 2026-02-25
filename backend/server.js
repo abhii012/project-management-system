345e2a89a2f409c52710d610faf9a54b37ead739
abhii012 <you@example.com>
2026-02-25 14:02:21 +0530

    fix backend routes

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const productRoutes=require('./routes/productRoutes')
+
 const app=express()
+app.use(express.json());
 app.use(cors({
   origin: "*"
 }));
-app.use(express.json());
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
